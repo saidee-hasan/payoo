3d0587a7ae85c8ff1d4d06605c2d4c792645a06a
saideeWD <mdsaideehasan@gmail.com>
2024-09-23 08:46:55 +0600

    money add

--- js/home.js
+++ js/home.js
@@ -3,6 +3,13 @@ document.getElementById("btn-addMoney").addEventListener('click',function(e){
    const addMoneyInput = document.getElementById("amount-add").value;
    const pinNumber =document.getElementById("pin-number").value;
    console.log(addMoneyInput,pinNumber)
-
+if(pinNumber === '1234'){
+    const balance = document.getElementById("balance").innerText;
+     const newBalance = parseFloat( balance) + parseFloat(addMoneyInput) ;
+     console.log(newBalance);
+     document.getElementById("balance").innerText = newBalance;
+}else{
+    alert("Failed to add money")
+}
 
 })
